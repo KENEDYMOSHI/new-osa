9275a813ff458fa609569bed13d0874f8418922d
KENEDYMOSHI <kenedymoshi21@gmail.com>
2025-12-31 23:36:46 +0300

    Sort license types alphabetically in applicant portal

--- src/app/pages/license-application/license-application.component.ts
+++ src/app/pages/license-application/license-application.component.ts
@@ -281,6 +281,16 @@ export class LicenseApplicationComponent implements OnInit {
             userSelectedInstruments: [] as string[]
           };
         });
+
+        // Sort alphabetically by name
+        this.licenseTypes.sort((a, b) => {
+            const nameA = a.name.toUpperCase();
+            const nameB = b.name.toUpperCase();
+            if (nameA < nameB) return -1;
+            if (nameA > nameB) return 1;
+            return 0;
+        });
+
         console.log('License types loaded:', this.licenseTypes);
         this.updateRequiredDocuments(); 
       },
